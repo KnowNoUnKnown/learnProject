abe6d01340b2bc9e0de894d4a7259c66bc171c8a
oraclexing <1425630563@qq.com>
2019-05-24 17:58:10 +0800

    提交

--- src/test/java/Ttest.java
+++ src/test/java/Ttest.java
@@ -1,4 +1,7 @@
+import com.google.common.collect.Lists;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Created by liuyong
@@ -9,5 +12,14 @@ public class Ttest {
 
     @Test
     public void TClassTest(){
+        List<String> strings = Lists.newArrayList("1","2","3");
+
+        List<Integer> integers = Lists.newArrayList(1,2,3,4,5);
+
+        System.out.println(strings.getClass().getName());
+        System.out.println(strings.get(0).getClass().getName());
+        System.out.println(integers.get(0).getClass().getName());
+        System.out.println(strings.getClass().equals(integers.getClass()));
+
      }
 }
